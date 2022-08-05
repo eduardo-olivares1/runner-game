8884378e0aea3f1d788405a601cee88aafbf11f6
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-05 13:11:22 -0700

    inital commit

---
+++ app/static/src/js/game.js
@@ -0,0 +1,93 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'phaser-game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 250 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+
+const game = new Phaser.Game(config);
+
+//PhaserJS requires function declarations
+function preload() {
+    this.load.setBaseURL(window.location.origin);
+    this.load.image('sky', './static/assets/sky.png');
+    this.load.image('ground', './static/assets/platform.png');
+    this.load.image('diamond', './static/assets/diamond.png');
+    this.load.spritesheet('woof', './static/assets/woof.png',
+        { frameWidth: 32, frameHeight: 32 }
+    );
+}
+
+function create() {
+    this.add.sprite(400, 300, 'sky');
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(100, 450, 'woof');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('woof', { start: 0, end: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'woof', frame: 2 }],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('woof', { start: 2, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(player, platforms);
+}
+
+function update() {
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-330);
+    }
+}
