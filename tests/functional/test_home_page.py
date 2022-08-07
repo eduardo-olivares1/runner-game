96ade409fde1109229e840bc2cd5432bef52a111
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-06 17:00:12 -0700

    update tests

--- tests/functional/test_home_page.py
+++ tests/functional/test_home_page.py
@@ -12,19 +12,9 @@ class HomePageTest(SeleniumTestCase):
     def test_correct_nav_bar(self):
         nav_bar = self.browser.find_element(By.XPATH, "//nav[1]")
         first_nav_item = nav_bar.find_element(By.XPATH, "./ul/li[1]/a")
-        second_nav_item = nav_bar.find_element(By.XPATH, "./ul/li[2]/a")
 
-        self.assertEqual("Home", first_nav_item.text)
+        self.assertEqual("Voz Games", first_nav_item.text)
         self.assertEqual("http://localhost:5000/", first_nav_item.get_attribute("href"))
-
-        self.assertEqual("WTF Forms Example", second_nav_item.text)
-        self.assertEqual(
-            "http://localhost:5000/form", second_nav_item.get_attribute("href")
-        )
-
-    def test_user_sees_welcome_message(self):
-        welcome_message = self.browser.find_element("id", "welcome-message")
-        self.assertEqual("Hello", welcome_message.text)
 
     def test_correct_footer(self):
         footer = self.browser.find_element(By.XPATH, "//footer")
