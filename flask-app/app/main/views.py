dc4fe2f51654bf7c7f5e0e846effe351dcf6261c
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-09 22:07:04 -0700

    remove static route

--- flask-app/app/main/views.py
+++ flask-app/app/main/views.py
@@ -1,12 +1,7 @@
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, current_app
 from . import main
 
 
 @main.route("/")
 def home():
     return render_template("main/index.html")
-
-
-@main.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory("static", filename)
