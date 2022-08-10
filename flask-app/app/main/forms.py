083a7c64a6cfbf3c9b3169f11e0c2e693ce63ae7
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-09 22:07:13 -0700

    remove model

--- flask-app/app/main/forms.py
+++ flask-app/app/main/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, EmailField
 from wtforms.validators import InputRequired, EqualTo, Email
 from sqlalchemy.exc import OperationalError
-from ..models import ExampleModel
 
 
 class ExampleForm(FlaskForm):
