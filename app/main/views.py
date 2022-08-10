6076d2b561cb124b88848abefbbe3c43de16deb8
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-10 15:04:43 -0700

    Nginx (#2) (#3)
    * Nginx (#2)
    
    * move flask app into service folder
    
    * update ci working directory
    
    * format w black
    
    * add guinicorn to req.txt
    
    * add nginx + compose scaffold
    
    * remove db references
    
    * format with black
    
    * remove static route
    
    * remove model
    
    * remove model
    
    * Update Dockerfile
    
    * rename nginx dockerfile
    
    * Update views.py
    
    * Update nginx.conf
    
    * update compose + flask dockerfile
    
    * Delete forms.py
    
    * remove ref to unused db and imports

--- app/main/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, redirect, url_for, flash, current_app
-from .forms import ExampleForm
-from . import main
-
-
-@main.route("/")
-def home():
-    return render_template("main/index.html")
