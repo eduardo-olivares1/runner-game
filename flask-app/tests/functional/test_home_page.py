a467ca39b3b432a6b34e21c0f17a424dab532892
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-10 21:19:40 -0700

    remove old home test

--- flask-app/tests/functional/test_home_page.py
+++ flask-app/tests/functional/test_home_page.py
@@ -8,14 +8,3 @@ class HomePageTest(SeleniumTestCase):
             self.skipTest("Selenium browser not available")
         else:
             self.browser.get("http://localhost:5000/")
-
-    def test_correct_nav_bar(self):
-        nav_bar = self.browser.find_element(By.XPATH, "//nav[1]")
-        first_nav_item = nav_bar.find_element(By.XPATH, "./ul/li[1]/a")
-
-        self.assertEqual("Voz Games", first_nav_item.text)
-        self.assertEqual("http://localhost:5000/", first_nav_item.get_attribute("href"))
-
-    def test_correct_footer(self):
-        footer = self.browser.find_element(By.XPATH, "//footer")
-        self.assertEqual("© Copyright Your Entity", footer.text)
