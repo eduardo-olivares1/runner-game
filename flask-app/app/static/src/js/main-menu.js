31561c7f5a4d96c98facb45c0c964c2a9f9d3d5e
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2022-08-19 15:57:34 -0700

    change bounce final y position

--- flask-app/app/static/src/js/main-menu.js
+++ flask-app/app/static/src/js/main-menu.js
@@ -4,7 +4,7 @@ class MainMenu {
         let nameLabel = this.add.text(400, -50, 'Tea Zone', { font: '50px Silkscreen', fill: 'black' });
         nameLabel.setOrigin(0.5, 0.5);
 
-        this.tweens.add({ targets: nameLabel, y: 80, duration: 1000, ease: 'bounce.out' })
+        this.tweens.add({ targets: nameLabel, y: 200, duration: 1000, ease: 'bounce.out' })
 
         let startText = 'press <<Enter>> to start';
         let startLabel = this.add.text(400, 300, startText, { font: '25px Silkscreen', fill: 'black' });
